a056eb5fa278cb1493a36d1e1b45e9cadf9388e8
davidcopperfield1991 <david.moh1991@gmail.com>
2022-09-10 17:59:36 +0430

    read json from post method

--- internal/http/http.go
+++ internal/http/http.go
@@ -1,10 +1,31 @@
 package http
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
+
+type Message struct {
+	Text   string `json:"text"`
+	Sender string `json:"sender"`
+	To     string `json:"to"`
+}
+
+func sendMessage(c *fiber.Ctx) error {
+	data := Message{}
+	err := c.BodyParser(&data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(data.Text)
+	fmt.Println(data.Sender)
+	fmt.Println(data.To)
+
+	return c.JSON(data)
+}
 
 func RunFiber() {
 	app := fiber.New()
@@ -13,5 +34,8 @@ func RunFiber() {
 		return c.SendString("message")
 	})
 
+	app.Post("/send/:message/:sender/:to", sendMessage)
+
 	log.Fatal(app.Listen(":6060"))
+
 }
